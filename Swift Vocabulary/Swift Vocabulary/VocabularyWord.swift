d09e04473d310826592f3d6c4e1939210e97082c
Galobawi Salih <galobawi-salih@lambdastudents.com>
2020-04-13 20:20:15 -0400

    I finished as much as i could

---
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyWord.swift
+//  Swift Vocabulary
+//
+//  Created by Lambda School on 4/13/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+struct VocabularyWord {
+    let word: String
+    let definition: String
+}
